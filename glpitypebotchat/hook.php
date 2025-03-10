716bd78e3694e3ac1cf69250323e3eaa39610b80
Yefclub <plataformas@yefclub.com.br>
2025-03-10 03:15:43 -0300

    Adiciona inclusão de dependências e melhora segurança na instalação do plugin

--- glpitypebotchat/hook.php
+++ glpitypebotchat/hook.php
@@ -1,9 +1,14 @@
 <?php
 
+include ("../../../inc/includes.php");
+
 /**
  * Função chamada durante a instalação do plugin
  */
 function plugin_glpitypebotchat_install() {
+    if (!class_exists('PluginGlpitypebotchatConfig')) {
+        require_once(GLPI_ROOT . '/plugins/glpitypebotchat/inc/config.class.php');
+    }
     PluginGlpitypebotchatConfig::install();
     PluginGlpitypebotchatMenu::addRightsToSession();
     return true;
@@ -13,6 +18,9 @@ function plugin_glpitypebotchat_install() {
  * Função chamada durante a desinstalação do plugin
  */
 function plugin_glpitypebotchat_uninstall() {
+    if (!class_exists('PluginGlpitypebotchatConfig')) {
+        require_once(GLPI_ROOT . '/plugins/glpitypebotchat/inc/config.class.php');
+    }
     PluginGlpitypebotchatConfig::uninstall();
     PluginGlpitypebotchatMenu::removeRightsFromSession();
     return true;
@@ -22,6 +30,9 @@ function plugin_glpitypebotchat_uninstall() {
  * Função chamada durante a purga do plugin
  */
 function plugin_glpitypebotchat_purge() {
+    if (!class_exists('PluginGlpitypebotchatConfig')) {
+        require_once(GLPI_ROOT . '/plugins/glpitypebotchat/inc/config.class.php');
+    }
     PluginGlpitypebotchatConfig::uninstall();
     PluginGlpitypebotchatMenu::removeRightsFromSession();
     return true;
